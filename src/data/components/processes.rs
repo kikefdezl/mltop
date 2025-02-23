d47718edc9b4ff7e89dcebb691b4b0c01518f925
kike <enriquelagui@outlook.com>
2025-02-23 15:02:34 +0100

    Add MEM/CPU usage info to processes

--- src/data/components/processes.rs
+++ src/data/components/processes.rs
@@ -3,16 +3,18 @@ use std::vec::IntoIter;
 use sysinfo::{Pid, System};
 
 #[derive(Clone)]
-pub enum GpuProcessType {
-    GRAPHIC,
-    COMPUTE,
+pub enum ProcessType {
+    CPU, // TODO: Add cpu processes
+    GPU_GRAPHIC,
+    GPU_COMPUTE,
 }
 
-impl ToString for GpuProcessType {
+impl ToString for ProcessType {
     fn to_string(&self) -> String {
         match self {
-            GpuProcessType::GRAPHIC => "GRAPHIC".to_string(),
-            GpuProcessType::COMPUTE => "COMPUTE".to_string(),
+            ProcessType::CPU => "".to_string(),
+            ProcessType::GPU_GRAPHIC => "GRAPHIC".to_string(),
+            ProcessType::GPU_COMPUTE => "COMPUTE".to_string(),
         }
     }
 }
@@ -20,8 +22,14 @@ impl ToString for GpuProcessType {
 #[derive(Clone)]
 pub struct Process {
     pub pid: u32,
-    pub type_: GpuProcessType,
+    pub type_: ProcessType,
     pub command: String,
+    // percentage 0-100% (can be higher than 100% if uses more than one core)
+    pub cpu_usage: f32,
+    // bytes
+    pub memory: u64,
+    // percentage 0-100%
+    pub memory_usage: f32,
 }
 
 #[derive(Clone)]
@@ -38,28 +46,38 @@ impl Processes {
 
         let mut processes = Vec::new();
 
+        let total_memory = sys.total_memory();
+
         processes.extend(device.running_compute_processes()?.iter().filter_map(|x| {
-            match sys.process(Pid::from(x.pid as usize)).unwrap().exe() {
+            match sys.process(Pid::from(x.pid as usize)) {
                 None => None,
-                Some(exe) => Some(Process {
-                    pid: x.pid,
-                    type_: GpuProcessType::COMPUTE,
-                    command: String::from(exe.to_str().unwrap()),
+                Some(p) => Some({
+                    let memory = p.memory();
+                    Process {
+                        pid: x.pid,
+                        type_: ProcessType::GPU_COMPUTE,
+                        command: String::from(p.exe().unwrap().to_str().unwrap()),
+                        memory,
+                        memory_usage: (memory as f32 / total_memory as f32) * 100.0,
+                        cpu_usage: p.cpu_usage(),
+                    }
                 }),
             }
         }));
-        processes.extend(device.running_graphics_processes()?.iter().map(|x| {
-            Process {
-                pid: x.pid,
-                type_: GpuProcessType::GRAPHIC,
-                command: String::from(
-                    sys.process(Pid::from(x.pid as usize))
-                        .unwrap()
-                        .exe()
-                        .unwrap()
-                        .to_str()
-                        .unwrap(),
-                ),
+        processes.extend(device.running_graphics_processes()?.iter().filter_map(|x| {
+            match sys.process(Pid::from(x.pid as usize)) {
+                None => None,
+                Some(p) => Some({
+                    let memory = p.memory();
+                    Process {
+                        pid: x.pid,
+                        type_: ProcessType::GPU_GRAPHIC,
+                        command: String::from(p.exe().unwrap().to_str().unwrap()),
+                        memory,
+                        memory_usage: (memory as f32 / total_memory as f32) * 100.0,
+                        cpu_usage: p.cpu_usage(),
+                    }
+                }),
             }
         }));
         Ok(Processes(processes))
