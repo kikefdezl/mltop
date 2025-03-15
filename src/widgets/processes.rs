a5d6fd22f9078a1bdc0bdc437f18e9ce5f4a6c58
kike <enriquelagui@outlook.com>
2025-03-15 23:13:49 +0100

    Include all processes in process-table

--- src/widgets/processes.rs
+++ src/widgets/processes.rs
@@ -1,6 +1,6 @@
-use crate::data::components::processes::Processes;
 use crate::constants::BYTES_PER_MB;
-
+use crate::data::components::processes::Processes;
+use itertools::Itertools;
 use ratatui::{
     buffer::Buffer,
     layout::{Alignment, Constraint, Rect},
@@ -20,30 +20,30 @@ impl ProcessesWidget {
 }
 
 impl Widget for ProcessesWidget {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let header_style = Style::default()
-            .fg(Color::Black)
-            .bg(Color::White);
+    fn render(mut self, area: Rect, buf: &mut Buffer) {
+        let header_style = Style::default().fg(Color::Black).bg(Color::White);
 
-        let header = ["   pid", "type", " CPU%", " MEM%", "      MEM", "Command"]
+        let header = ["   pid", "type", " CPU%", "  MEM%", "      MEM", "Command"]
             .into_iter()
             .map(Cell::from)
             .collect::<Row>()
             .style(header_style)
             .height(1);
 
+        self.data.0.sort_by_key(|p| (p.cpu_usage * 1000.0) as u32);
         let rows: Vec<Row> = self
             .data
             .into_iter()
+            .sorted_by(|a, b| b.cpu_usage.total_cmp(&a.cpu_usage))
             .map(|data| {
                 Row::new(vec![
                     Cell::from(Text::from(data.pid.to_string()).alignment(Alignment::Right)),
                     Cell::from(Text::from(data.type_.to_string())),
                     Cell::from(
-                        Text::from(format!("{:.0}%", data.cpu_usage)).alignment(Alignment::Right),
+                        Text::from(format!("{:.1}%", data.cpu_usage)).alignment(Alignment::Right),
                     ),
                     Cell::from(
-                        Text::from(format!("{:.0}%", data.memory_usage))
+                        Text::from(format!("{:.1}%", data.memory_usage))
                             .alignment(Alignment::Right),
                     ),
                     Cell::from(
@@ -61,7 +61,7 @@ impl Widget for ProcessesWidget {
                 Constraint::Length(6),
                 Constraint::Length(8),
                 Constraint::Length(5),
-                Constraint::Length(5),
+                Constraint::Length(6),
                 Constraint::Length(9),
                 Constraint::Min(10),
             ],
